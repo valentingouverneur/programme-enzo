776271a3ec93cbccc9154a2490284def9ed42b41
valentingouverneur <valentin.gouverneur@gmail.com>
2025-03-25 16:52:54 +0100

    distance semaine

---
+++ app/components/WeekStats.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex gap-6">
+        <div>
+            <p class="text-gray-400 text-sm">Distance totale</p>
+            <p class="text-white font-semibold" x-text="calculerDistanceTotale() + ' km'">26 km</p>
+        </div>
+        <div>
+            <p class="text-gray-400 text-sm">Durée totale</p>
+            <p class="text-white font-semibold" x-text="calculerDureeTotale() + ' min'">155 min</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        semaine: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        calculerDistanceTotale() {
+            return this.semaine.seances.reduce((total, seance) => total + seance.distance, 0);
+        },
+        calculerDureeTotale() {
+            return this.semaine.seances.reduce((total, seance) => total + seance.duree, 0);
+        }
+    }
+}
+</script> 
